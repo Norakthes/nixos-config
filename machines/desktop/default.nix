58fcc9f5a7cd0d775f1dea3f33886130be7929b3
Rasmus Bødker <rasmus.b.bodker@gmail.com>
2025-07-28 01:17:15 +0200

    Fix dpi scaling on display, and use integer scaling only

--- machines/desktop/default.nix
+++ machines/desktop/default.nix
@@ -17,12 +17,10 @@
     [Seat:*]
     position=50%,center
     display-setup-script=${pkgs.writeScript "lightdm-display-setup" ''
-      #!${pkgs.bash}/bin/bash
-
       ${pkgs.xorg.xrandr}/bin/xrandr \
-        --output DP-3   --mode 1920x1080 --rate 60  --pos 0x0 \
+        --output DP-3   --mode 1920x1080 --rate 60  --pos 0x249 \
         --output DP-1   --mode 2560x1440 --rate 165 --pos 1920x0 --primary \
-        --output HDMI-1 --mode 1920x1080 --rate 60  --pos 4480x0
+        --output HDMI-1 --mode 1920x1080 --rate 60  --pos 4480x326    
     ''}
   '';
 }
